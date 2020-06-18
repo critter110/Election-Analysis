5c619867cdbb45c0858ec394b30edceb12efc396
Chris Ritter <chrisritter@Chriss-MacBook-Pro.local>
2020-06-18 16:47:22 -0500

    Adding PyPoll.py file.

---
+++ pythonExample.py
@@ -0,0 +1,15 @@
+# Variable
+exampleString = "Hello World"
+print(exampleString)
+print(type(exampleString))
+
+myName = "Chris Ritter"
+myAge = 23
+myBirthDay = "05/11/1997"
+
+myIntro = f"Hello my name is {myName} and I am {myAge}. I was born on {myBirthDay}." #String Interpolation
+print(myIntro)
+
+#Lists
+
+listOfDifferentTypes = [0,1.0, "Some String", [0,1,2,3]]
